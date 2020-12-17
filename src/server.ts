97c9f62f25d4b102571ee5e993806420b807b758
Cristian Tala S <yomismo@cristiantala.cl>
2020-12-17 10:33:04 -0300

    Deleted Feeds

--- src/server.ts
+++ src/server.ts
@@ -2,15 +2,15 @@ import cors from 'cors';
 import express from 'express';
 import {sequelize} from './sequelize';
 
-import {IndexRouter} from './controllers/v0/index.router';
+// import {IndexRouter} from './controllers/v0/index.router';
+import {UserRouter} from './controllers/v0/users/routes/user.router';
 
 import bodyParser from 'body-parser';
 import {config} from './config/config';
-import {V0_FEED_MODELS, V0_USER_MODELS} from './controllers/v0/model.index';
+import {V0_USER_MODELS} from './controllers/v0/model.index';
 
 
 (async () => {
-  await sequelize.addModels(V0_FEED_MODELS);
   await sequelize.addModels(V0_USER_MODELS);
   await sequelize.sync();
 
@@ -29,12 +29,12 @@ import {V0_FEED_MODELS, V0_USER_MODELS} from './controllers/v0/model.index';
     origin: '*',
   }));
 
-  app.use('/api/v0/', IndexRouter);
+  app.use('/', UserRouter);
 
   // Root URI call
-  app.get( '/', async ( req, res ) => {
-    res.send( '/api/v0/' );
-  } );
+  // app.get( '/', async ( req, res ) => {
+  //   res.send( '/api/v0/' );
+  // } );
 
 
   // Start the Server
